5379364e9132a117f32067d78c6cd1564b9aba30
CHENK <CHENK@CHENK-PC>
2016-02-26 17:53:26 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

---
+++ src/main/java/net/simpleframework/common/Pinyin.java
@@ -0,0 +1,45 @@
+package net.simpleframework.common;
+
+import net.simpleframework.common.logger.Log;
+import net.simpleframework.common.logger.LogFactory;
+import net.sourceforge.pinyin4j.PinyinHelper;
+import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
+import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinVCharType;
+import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
+
+public abstract class Pinyin {
+
+	public static String toPinYin(final String src) {
+		char[] t1 = null;
+		t1 = src.toCharArray();
+		// System.out.println(t1.length);
+		String[] t2 = new String[t1.length];
+		// System.out.println(t2.length);
+		// 设置汉字拼音输出的格式
+		final HanyuPinyinOutputFormat t3 = new HanyuPinyinOutputFormat();
+		t3.setCaseType(HanyuPinyinCaseType.LOWERCASE);
+		t3.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
+		t3.setVCharType(HanyuPinyinVCharType.WITH_V);
+		String t4 = "";
+		final int t0 = t1.length;
+		try {
+			for (int i = 0; i < t0; i++) {
+				// 判断能否为汉字字符
+				if (Character.toString(t1[i]).matches("[\\u4E00-\\u9FA5]+")) {
+					t2 = PinyinHelper.toHanyuPinyinStringArray(t1[i], t3);// 将汉字的几种全拼都存到t2数组中
+					t4 += t2[0];// 取出该汉字全拼的第一种读音并连接到字符串t4后
+				} else {
+					// 如果不是汉字字符，间接取出字符并连接到字符串t4后
+					t4 += Character.toString(t1[i]);
+				}
+			}
+		} catch (final BadHanyuPinyinOutputFormatCombination e) {
+			log.warn(e);
+		}
+		return t4;
+	}
+
+	static Log log = LogFactory.getLogger(Pinyin.class);
+}
