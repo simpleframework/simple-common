85442334ff5a540f14e69e62603bacfa0745c931
CHENK <CHENK@CHENK-PC>
2016-04-28 19:52:14 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

--- src/main/java/net/simpleframework/common/IoUtils2.java
+++ src/main/java/net/simpleframework/common/IoUtils2.java
@@ -4,11 +4,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
-import com.caucho.hessian.io.HessianOutput;
-import com.esotericsoftware.kryo.Kryo;
-import com.esotericsoftware.kryo.io.ByteBufferInput;
-import com.esotericsoftware.kryo.io.ByteBufferOutput;
-
 /**
  * Licensed under the Apache License, Version 2.0
  * 
@@ -19,25 +14,27 @@ import com.esotericsoftware.kryo.io.ByteBufferOutput;
 public abstract class IoUtils2 {
 
 	static byte[] kryo_serialize(final Object kryo, final Object obj) {
-		final ByteBufferOutput buffer = new ByteBufferOutput(1024, -1);
-		((Kryo) kryo).writeClassAndObject(buffer, obj);
+		final com.esotericsoftware.kryo.io.ByteBufferOutput buffer = new com.esotericsoftware.kryo.io.ByteBufferOutput(
+				1024, -1);
+		((com.esotericsoftware.kryo.Kryo) kryo).writeClassAndObject(buffer, obj);
 		return buffer.toBytes();
 	}
 
 	static byte[] hessian_serialize(final Object obj) throws IOException {
 		final ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		final HessianOutput ho = new HessianOutput(bos);
+		final com.caucho.hessian.io.HessianOutput ho = new com.caucho.hessian.io.HessianOutput(bos);
 		ho.writeObject(obj);
 		return bos.toByteArray();
 	}
 
 	static Object kryo_deserialize(final Object kryo, final byte[] bytes, final Class<?> typeClass) {
-		final Kryo _kryo = (Kryo) kryo;
+		final com.esotericsoftware.kryo.Kryo _kryo = (com.esotericsoftware.kryo.Kryo) kryo;
 		if (typeClass != null) {
 			_kryo.register(typeClass);
-			return _kryo.readObject(new ByteBufferInput(bytes), typeClass);
+			return _kryo
+					.readObject(new com.esotericsoftware.kryo.io.ByteBufferInput(bytes), typeClass);
 		} else {
-			return _kryo.readClassAndObject(new ByteBufferInput(bytes));
+			return _kryo.readClassAndObject(new com.esotericsoftware.kryo.io.ByteBufferInput(bytes));
 		}
 	}
 
