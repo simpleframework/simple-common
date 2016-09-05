437b641699d9fb1055ff92e97a2174193a69649d
CHENK <CHENK@CHENK-PC>
2016-09-05 17:25:13 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

--- src/main/java/net/simpleframework/common/jedis/JedisVSet.java
+++ src/main/java/net/simpleframework/common/jedis/JedisVSet.java
@@ -9,7 +9,8 @@ import redis.clients.jedis.JedisPool;
 /**
  * Licensed under the Apache License, Version 2.0
  * 
- * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
+ * @author 陈侃(cknet@126.com, 13910090885)
+ *         https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 public class JedisVSet extends HashSet<String> {
